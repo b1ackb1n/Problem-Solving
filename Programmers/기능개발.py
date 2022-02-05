a9be9fb31811bbe3761189f1f46b98e392d405a6
b1ackb1n <wateryb1n@gmail.com>
2022-02-06 04:30:44 +0900

    math.ceil(), Queue 풀이 모두 중요!

--- Programmers/기능개발.py
+++ Programmers/기능개발.py
@@ -1,14 +1,36 @@
+'''
+# math.ceil 이용한 풀이
 import math
 def solution(progresses, speeds):
-    ans = []
-    num = 1
-    time = math.ceil((100-progresses[0])/speeds[0])
-    for i in range(1, len(progresses)):
-        if time >= math.ceil((100-progresses[i])/speeds[i]):
-            num += 1
+    n = math.ceil((100-progresses[0])/speeds[0])
+    cnt = 1
+    answer = []
+    for i in range (1, len(speeds)):
+        if progresses[i]+speeds[i]*n >= 100:
+            cnt += 1
         else:
-            ans.append(num)
-            num = 1
-            time = math.ceil((100-progresses[i])/speeds[i])
-    ans.append(num)
-    return ans
+            answer.append(cnt)
+            n = math.ceil((100-progresses[i])/speeds[i])
+            cnt = 1
+    answer.append(cnt) #이걸 빼먹었다..
+    return answer
+'''
+
+# queue를 이용한 풀이
+import math
+def solution(progresses, speeds):
+    time, cnt = 0, 0
+    answer = []
+    while len(progresses)>0:
+        if progresses[0]+speeds[0]*time >= 100:
+            progresses.pop(0)
+            speeds.pop(0)
+            cnt+=1
+        else:
+            if cnt>0: 
+                answer.append(cnt)
+                cnt=0
+            time+=1
+            
+    answer.append(cnt)
+    return answer
