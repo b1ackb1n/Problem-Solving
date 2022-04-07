0a4e10320967508a7cd2a07db74a84eb0988fc57
b1ackb1n <wateryb1n@gmail.com>
2022-04-08 03:34:14 +0900

    앞자리 0인수 int(), 앞 세자리 기준 정렬 sort(key=lambda x:x*3)

--- Programmers/가장 큰 수.py
+++ Programmers/가장 큰 수.py
@@ -2,17 +2,11 @@
 # 시간초과 26.7/100
 from itertools import permutations
 def solution(numbers):
-    answer = ''
-    arr, nums = [], []
-    tmp = list(permutations(numbers))
-    for t in tmp:
-        arr.append(list(map(str, t)))
-    print(arr)
-    
-    for a in arr:
-        nums.append(int(''.join(a)))
-
-    return str(sorted(nums)[-1])
+    answer = []
+    tmp = list(permutations(numbers, len(numbers)))
+    for i in tmp:
+        answer.append(int(''.join(map(str, i))))
+    return str(max(answer))
 '''
 
 def solution(numbers):
